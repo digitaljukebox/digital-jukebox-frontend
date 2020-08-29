64768e2d37313b146274a99919fddfd1e9179984
ThornM9 <tm.jalal@gmail.com>
2020-08-29 10:09:24 +1000

    load venues from firestore and compute distances to display sorted list

--- src/pages/NearbyVenues.vue
+++ src/pages/NearbyVenues.vue
@@ -10,7 +10,7 @@
         <q-item clickable v-ripple v-for="venue in venues" :key=venue.id>
           <q-item-section>
             <q-item-label>{{ venue.name }}</q-item-label>
-            <q-item-label caption>{{ venue.distanceTo }}</q-item-label>
+            <q-item-label caption>{{ getVenueDistance(venue.location) }}</q-item-label>
           </q-item-section>
           <q-icon name="chevron_right"/>
         </q-item>
@@ -28,15 +28,19 @@
 </style>
 
 <script lang="ts">
+import {NCoordinates, Venue, venueFromFirestoreDocument} from 'src/types.ts'
+
 export default {
   name: 'NearbyVenues',
   data() {
     const d: { 
       tab: string,
       venues: Venue[],
+      userLocation: NCoordinates,
     } = { 
       tab: 'list',
       venues: [],
+      userLocation: null,
     };
 
     return d;
@@ -44,19 +48,48 @@ export default {
   methods: {
     updateVenues() {
       // send the users location to the server to get an updated list of venues and distances
-      // TODO
       this.venues = [];
+      const db = this.$fb.getFirestore();
+      db.collection("venues").get().then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          this.venues.push(venueFromFirestoreDocument(doc));
+        });
+
+        this.venues.sort(function(a: Venue, b: Venue) {
+          let distA: number = this.userLocation.distanceTo(a.location);
+          let distB: number = this.userLocation.distanceTo(b.location);
+          return distA - distB;
+        })
+      });
+      
+
     },
     getUserLocation() {
+      let _this = this;
       navigator.geolocation.getCurrentPosition(function(position) {
-        console.log(position);
+        _this.userLocation = new NCoordinates(position.coords.latitude, position.coords.longitude);
+        
       })
+    },
+    getVenueDistance: function(venueLocation: NCoordinates): string {
+      if (this.userLocation) {
+        const distance :number = this.userLocation.distanceTo(venueLocation);
+        let units = 'km';
+        if (distance < 1) {
+          units = 'm';
+        }
+        return `${distance.toFixed(2)}${units}`;
+      } else {
+        return '????';
+      }
     }
   },
   mounted() {
     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
     this.getUserLocation();
-    // setInterval(this.updateVenues, 30000);
+
+    this.updateVenues();
+    setInterval(this.updateVenues, 30000);
   }
 }
 </script>
