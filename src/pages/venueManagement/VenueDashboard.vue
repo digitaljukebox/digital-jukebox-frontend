b3053522c1a8119471e0ca31db8593fec2932108
Aidan <akinzett@gmail.com>
2020-09-19 17:36:21 +1000

    added cards for the other graphs on the venue dashboard

--- src/pages/venueManagement/VenueDashboard.vue
+++ src/pages/venueManagement/VenueDashboard.vue
@@ -3,30 +3,52 @@
     <div class="row justify-evenly">
       <q-card class="data-card">
         <q-card-section>
-          Venue Profile Views
+          <div class="text-h6">Venue Profile Views</div>
         </q-card-section>
 
         <q-card-section>
           <div>
-          <canvas id="profile-views-chart" width="100%" height="100%" style="padding-bottom: 50px;"></canvas>
+            <canvas
+              id="profile-views-chart"
+              width="100%"
+              height="100%"
+              style="padding-bottom: 50px;"
+            ></canvas>
           </div>
         </q-card-section>
       </q-card>
       <q-card class="data-card">
         <q-card-section>
-          Daily Users
+          <div class="text-h6">Daily User Check-Ins</div>
         </q-card-section>
 
+        <q-card-section> </q-card-section>
+      </q-card>
+      <q-card class="data-card">
         <q-card-section>
-
+          <div class="text-h6">Daily User Engagment</div>
+          <div class="text-subtitle1 text-grey-6">
+            Number of requests per user
+          </div>
         </q-card-section>
+
+        <q-card-section> </q-card-section>
+      </q-card>
+      <q-card class="data-card">
+        <q-card-section>
+          <div class="text-h6">Daily Conversion Rate</div>
+          <div class="text-subtitle1 text-grey-6">
+            Check-ins relative to views
+          </div>
+        </q-card-section>
+
+        <q-card-section> </q-card-section>
       </q-card>
     </div>
   </div>
 </template>
 
 <style scoped>
-
 .data-card {
   margin: 25px;
   height: 20em;
@@ -46,8 +68,8 @@ export default {
   name: 'VenueDashboard',
   data() {
     return {
-      venueId: '',
-    }
+      venueId: ''
+    };
   },
   mounted() {
     this.venueId = this.$route.params.id;
@@ -58,25 +80,25 @@ export default {
     initProfileViewsChart() {
       let _this = this;
       db.collection('venueprofileviews').onSnapshot(querySnapshot => {
-        const venueProfileViews: VenueProfileView[] = []
-        
-        querySnapshot.forEach(doc =>{
+        const venueProfileViews: VenueProfileView[] = [];
+
+        querySnapshot.forEach(doc => {
           let data = doc.data();
           if (data.venueId == _this.venueId) {
-            let profileView : VenueProfileView = {
+            let profileView: VenueProfileView = {
               venueId: data.venueId,
               userId: data.userId,
-              timestamp: data.timestamp.toDate(),
-            }
-            venueProfileViews.push(profileView)
+              timestamp: data.timestamp.toDate()
+            };
+            venueProfileViews.push(profileView);
           }
         });
 
         type viewsBucket = {
-          [key: string]: number,
-        }
+          [key: string]: number;
+        };
 
-        let viewsPerDay : viewsBucket= {};
+        let viewsPerDay: viewsBucket = {};
         for (let i = 0; i < venueProfileViews.length; i++) {
           let view = venueProfileViews[i];
           let dateString = view.timestamp.toDateString();
@@ -91,13 +113,13 @@ export default {
         let minDate = new Date();
         let days = Object.keys(viewsPerDay);
         for (let i = 0; i < days.length; i++) {
-          let parsed = new Date(days[i])
+          let parsed = new Date(days[i]);
           if (parsed < minDate) {
             minDate = parsed;
           }
         }
-        
-        const chartData = []
+
+        const chartData = [];
         let now = new Date();
         for (let d = minDate; d <= now; d.setDate(d.getDate() + 1)) {
           chartData.push({
@@ -106,31 +128,36 @@ export default {
           });
         }
 
-
         console.log(chartData);
         const ctx = document.getElementById('profile-views-chart');
         const myChart = new Chart(ctx, {
           type: 'bar',
           data: {
-            datasets: [{
-              label: 'Daily Venue Views',
-              data: chartData,
-            }]
+            datasets: [
+              {
+                label: 'Daily Venue Views',
+                data: chartData
+              }
+            ]
           },
           options: {
             scales: {
-              xAxes: [{
-                type: 'time',
-                time: {
-                  unit: 'day'
+              xAxes: [
+                {
+                  type: 'time',
+                  time: {
+                    unit: 'day'
+                  }
                 }
-              }],
-              yAxes: [{
-                display: false,
-                ticks: {
-                  beginAtZero: true
+              ],
+              yAxes: [
+                {
+                  display: false,
+                  ticks: {
+                    beginAtZero: true
+                  }
                 }
-              }]
+              ]
             },
             legend: {
               display: false
@@ -145,10 +172,7 @@ export default {
           }
         });
       });
-      
-    
     }
   }
-}
+};
 </script>
-
