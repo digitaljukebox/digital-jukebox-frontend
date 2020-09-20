ac01838c0e27cfc398571a77275ebd67315df725
Jaimyn Mayer <hello@jaimyn.com.au>
2020-09-20 21:27:14 +1000

    Added allow and deny list UI

--- src/components/AllowDenyList.vue
+++ src/components/AllowDenyList.vue
@@ -1,5 +1,97 @@
 <template>
+  <div class="row q-col-gutter-md">
+    <div class="q-my-md col-sm-12 col-md-6">
+      <q-toolbar class="bg-primary text-white">
+        <q-toolbar-title>Allow List</q-toolbar-title>
+        <q-btn flat round @click="addAllowList = !addAllowList"><q-icon name="fas fa-plus"/></q-btn>
+      </q-toolbar>
 
+      <q-list bordered separator>
+        <q-item>
+          <q-item-section>
+            <span>The songs and artists in this list are the only songs allowed to be played. The Deny List will override anything specified here.</span>
+          </q-item-section>
+        </q-item>
+
+        <q-item v-if="addAllowList">
+          <q-item-section>
+            <q-input v-model="addDenyListItem" label="Search for a song or artist" />
+          </q-item-section>
+        </q-item>
+
+        <q-item v-if="!allowList.length">
+          <q-item-section>
+          There are no items in the Allow List.
+          </q-item-section>
+        </q-item>
+
+        <q-item v-for="element in allowList" :key="element.id">
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ element.name }}</q-item-label>
+            <q-item-label caption lines="1">Artist Name</q-item-label>
+            <q-item-label caption lines="1">2:56 mins</q-item-label>
+          </q-item-section>
+
+          <q-item-section side>
+            <div class="row items-center q-gutter-sm">
+              <q-btn flat round color="negative" icon="fas fa-times-circle" />
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+
+    <div class="q-my-md col-sm-12 col-md-6">
+      <q-toolbar class="bg-primary text-white">
+        <q-toolbar-title>Deny List</q-toolbar-title>
+        <q-btn flat round @click="addDenyList = !addDenyList"><q-icon name="fas fa-plus"/></q-btn>
+      </q-toolbar>
+
+      <q-list bordered separator>
+        <q-item>
+          <q-item-section>
+            <span>The songs and artists in this list will not be allowed to be played.</span>
+          </q-item-section>
+        </q-item>
+
+        <q-item v-if="addDenyList">
+          <q-item-section>
+            <q-input v-model="addAllowListItem" label="Search for a song or artist" />
+          </q-item-section>
+        </q-item>
+
+        <q-item v-if="!denyList.length">
+          <q-item-section>
+            There are no items in the Allow List.
+          </q-item-section>
+        </q-item>
+
+        <q-item v-for="element in denyList" :key="element.id">
+          <q-item-section avatar>
+            <q-avatar>
+              <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
+            </q-avatar>
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>{{ element.name }}</q-item-label>
+            <q-item-label caption lines="1">Artist Name</q-item-label>
+            <q-item-label caption lines="1">2:56 mins</q-item-label>
+          </q-item-section>
+
+          <q-item-section side>
+            <div class="row items-center q-gutter-sm">
+              <q-btn flat round color="negative" icon="fas fa-times-circle" />
+            </div>
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,11 +101,16 @@ import 'firebase/firestore';
 const db = firebase.firestore();
 
 export default defineComponent({
-  name: 'CreateVenue',
+  name: 'AllowDenyList',
   data() {
     return {
-      AllowList: [],
-      DenyList: []
+      addAllowList: false,
+      addDenyList: false,
+      allowList: [],
+      denyList: [
+      { id: 1, name: 'hi' },
+      { id: 2, name: 'hello' }
+    ]
     };
   },
   created() {
--- src/pages/venueManagement/AllowDenyPage.vue
+++ src/pages/venueManagement/AllowDenyPage.vue
@@ -4,39 +4,8 @@
       {{ venue.name }}
     </div>
 
-    <div class="q-my-md" style="max-width: 500px;">
-      <q-toolbar class="bg-primary text-white">
-        <q-toolbar-title>Upcoming Songs</q-toolbar-title>
-        <q-btn flat round><q-icon name="fas fa-plus"/></q-btn>
-      </q-toolbar>
-      <q-list bordered>
-        <draggable v-model="myArray" @start="drag = true" @end="drag = false">
-          <q-item v-for="element in myArray" :key="element.id">
-            <q-item-section avatar>
-              <q-avatar>
-                <img src="https://cdn.quasar.dev/img/boy-avatar.png" />
-              </q-avatar>
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>{{ element.name }}</q-item-label>
-              <q-item-label caption lines="1">Artist Name</q-item-label>
-              <q-item-label caption lines="1">2:56 mins</q-item-label>
-            </q-item-section>
+    <allow-deny-list></allow-deny-list>
 
-            <q-item-section>
-              <q-item-label caption>Requested By: Aidan K.</q-item-label>
-            </q-item-section>
-
-            <q-item-section side>
-              <div class="row items-center q-gutter-sm">
-                <q-btn flat round color="negative" icon="fas fa-minus-circle" />
-                <q-icon name="fas fa-grip-lines" size="sm" color="grey-5" />
-              </div>
-            </q-item-section>
-          </q-item>
-        </draggable>
-      </q-list>
-    </div>
   </q-page>
 </template>
 
@@ -45,22 +14,19 @@ import firebase from 'firebase';
 import 'firebase/firestore';
 const db = firebase.firestore();
 import draggable from 'vuedraggable';
+import AllowDenyList from '../../components/AllowDenyList';
 
 export default {
   name: 'VenueDetails',
   components: {
-    draggable
+    draggable, AllowDenyList
   },
   data() {
     return {
       venueId: null,
       venue: {
         name: null
-      },
-      myArray: [
-        { id: 1, name: 'hi' },
-        { id: 2, name: 'hello' }
-      ]
+      }
     };
   },
   mounted() {
