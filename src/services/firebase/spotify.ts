9e1ed9bdd94c627485cb95eb74310448f1599a87
Jaimyn <github@jaimyn.com.au>
2020-10-25 12:59:55 +1000

    implemented spotify access token refreshing

--- src/services/firebase/spotify.ts
+++ src/services/firebase/spotify.ts
@@ -6,6 +6,7 @@ import SpotifyWebApi from 'spotify-web-api-node';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 import serverMethods from 'spotify-web-api-node/src/server-methods';
+import { UserProfile } from 'src/types';
 // eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore
 SpotifyWebApi._addMethods(serverMethods);
@@ -15,23 +16,76 @@ const redirectUri = `${window.location.protocol}//${window.location.host}/spotif
 const clientId = '18aac906abf4434197d6280aca076f45';
 const sId = '2a8bf2753891411b977d1ebf912e3cfc';
 
-export const spotifyApi = new SpotifyWebApi({
-  redirectUri: redirectUri,
-  clientId: clientId,
-  clientSecret: sId,
-});
-
 export function authenticateSpotify() {
-  window.location.href = spotifyApi.createAuthorizeURL(scopes, '')
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: redirectUri,
+    clientId: clientId,
+    clientSecret: sId,
+  });
 
+  window.location.href = spotifyApi.createAuthorizeURL(scopes, '')
+}
+
+export async function refreshSpotifyToken() {
+  return new Promise((resolve, reject) => {
+    firebase.auth().onAuthStateChanged(async user => {
+      if (user) {
+        const db = firebase.firestore();
+        db.collection('users').doc(user.uid).onSnapshot(async doc => {
+          const userProfile = await doc.data() as UserProfile;
+
+          const spotifyApi = new SpotifyWebApi({
+            redirectUri: redirectUri,
+            clientId: clientId,
+            clientSecret: sId,
+            refreshToken: userProfile.spotify.refreshToken,
+          });
+
+          console.log(userProfile.spotify.refreshToken);
+
+          spotifyApi.refreshAccessToken().then((data) => {
+              console.log(data);
+              firebase.auth().onAuthStateChanged(async user => {
+                let firebaseUser: firebase.User;
+                if (user) {
+                  firebaseUser = { ...user };
+                }
+
+                if (firebaseUser) {
+                  await db.collection('users')
+                    .doc(firebaseUser.uid)
+                    .update({ spotify: {
+                        accessToken: data.body.access_token,
+                        expiresAt: Math.floor(Date.now() / 1000 + data.body['expires_in'])
+                      } })
+                }
+                resolve(data.body.access_token)
+              });
+
+              // Set the access token on the API object to use it in later calls
+              spotifyApi.setAccessToken(data.body['access_token']);
+            },
+            function(err) {
+              console.log('Something went wrong!', err);
+              reject(err);
+            }
+          );
+        });
+      }
+    });
+  })
 }
 
 export async function spotifyCallbackRoute(code: string) {
-  const db = firebase.firestore();
+  const spotifyApi = new SpotifyWebApi({
+    redirectUri: redirectUri,
+    clientId: clientId,
+    clientSecret: sId,
+  });
 
   await spotifyApi.authorizationCodeGrant(code).then(
     function(data) {
-
+      const db = firebase.firestore();
       firebase.auth().onAuthStateChanged(async user => {
         let firebaseUser: firebase.User;
         if (user) {
