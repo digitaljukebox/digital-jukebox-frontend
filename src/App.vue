9f43f114b67cf0ec98ffcf09958a0fcdf269a7f0
Jaimyn <github@jaimyn.com.au>
2020-10-25 12:59:16 +1000

    added axios interceptor for spotify API requests

--- src/App.vue
+++ src/App.vue
@@ -8,9 +8,18 @@
 <script lang="ts">
 import { defineComponent } from '@vue/composition-api';
 import 'assets/main.scss';
+import { refreshSpotifyToken } from 'src/services/firebase/spotify';
 
 export default defineComponent({
-  name: 'App'
+  name: 'App',
+  beforeCreate() {
+    this.$axios.interceptors.response.use((response) => response, (error) => {
+      // Do something with response error
+      if (error.response.status === 401 && error.response.config.url.includes('api.spotify.com')) {
+        refreshSpotifyToken();
+      }
+    });
+  }
 });
 </script>
 
