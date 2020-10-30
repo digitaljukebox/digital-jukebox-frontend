ed8d479fdd3f41fd4e48a5696f053625a85d8a19
Aidan <akinzett@gmail.com>
2020-10-30 20:47:02 +1000

    added the other graph and more features to the venue dashboard page

--- src/pages/venueManagement/VenueDashboard.vue
+++ src/pages/venueManagement/VenueDashboard.vue
@@ -1,57 +1,57 @@
 <template>
   <div>
-    <div class="row justify-evenly">
+    <div class="row" style="margin: 30px; margin-top: 10px; ">
+      <div
+        class="q-gutter-sm"
+        style="display: flex; align-items: baseline; margin-right: 15px"
+      >
+        <span class="text-subtitle1" style="margin-right: 10px"
+          >Show data from the last:</span
+        >
+        <q-input
+          v-model.number="number"
+          type="number"
+          label=""
+          style="width: 3em;"
+        />
+      </div>
+
+      <div class="q-gutter-sm" style="display: flex; align-items: end">
+        <q-radio v-model="period" val="day" label="Days" />
+        <q-radio v-model="period" val="week" label="Weeks" />
+        <q-radio v-model="period" val="month" label="Months" />
+        <q-radio v-model="period" val="Year" label="Years" />
+      </div>
+    </div>
+    <div class="row justify-evenly" style="margin: 15px">
       <q-card class="data-card">
         <q-card-section>
           <div class="text-h6">Venue Profile Views</div>
         </q-card-section>
-
-        <q-card-section>
-          <div>
-            <canvas
-              id="profile-views-chart"
-              width="100%"
-              height="100%"
-              style="padding-bottom: 50px;"
-            ></canvas>
-          </div>
-        </q-card-section>
+        <div class="graph-container">
+          <canvas id="profile-views-chart" />
+        </div>
       </q-card>
       <q-card class="data-card">
         <q-card-section>
-          <div class="text-h6">Daily User Check-Ins</div>
+          <div class="text-h6">User Check-Ins</div>
         </q-card-section>
 
-        <q-card-section>
-          <div>
-            <canvas
-              id="check-ins-chart"
-              width="100%"
-              height="100%"
-              style="padding-bottom: 50px;"
-            ></canvas>
-          </div>
-        </q-card-section>
+        <div class="graph-container">
+          <canvas id="check-ins-chart" />
+        </div>
       </q-card>
       <q-card class="data-card">
         <q-card-section>
-          <div class="text-h6">Daily User Engagment</div>
+          <div class="text-h6">Conversion Rate</div>
           <div class="text-subtitle1 text-grey-6">
-            Number of requests per user
+            Number of check-ins per view
           </div>
         </q-card-section>
 
-        <q-card-section> </q-card-section>
-      </q-card>
-      <q-card class="data-card">
-        <q-card-section>
-          <div class="text-h6">Daily Conversion Rate</div>
-          <div class="text-subtitle1 text-grey-6">
-            Check-ins relative to views
-          </div>
-        </q-card-section>
-
-        <q-card-section> </q-card-section>
+        <div class="graph-container" style="height: 330px">
+          <canvas id="conversions-chart" />
+        </div>
       </q-card>
     </div>
   </div>
@@ -59,25 +59,84 @@
 
 <style scoped>
 .data-card {
-  margin: 25px;
-  height: 20em;
-  width: 20em;
+  margin: 10px;
+  height: 400px;
+  width: 400px;
+}
+
+.graph-container {
+  height: 350px;
+  padding: 10px;
+  padding-bottom: 30px;
 }
 </style>
 
 <script lang="ts">
-import firebase, { analytics } from 'firebase';
+import firebase from 'firebase';
 import 'firebase/firestore';
 import Chart from 'chart.js';
 import { VenueProfileView, VenueCheckIn } from 'src/types';
+import dayjs from 'dayjs';
 
 const db = firebase.firestore();
+
+type checkInBucket = {
+  [key: string]: number;
+};
+
+type viewsBucket = {
+  [key: string]: number;
+};
+
+const chartOptions: Chart.ChartOptions = {
+  responsive: true,
+  maintainAspectRatio: false,
+  scales: {
+    xAxes: [
+      {
+        type: 'time',
+        time: {
+          unit: 'day'
+        }
+      }
+    ],
+    yAxes: [
+      {
+        // display: false,
+        ticks: {
+          beginAtZero: true,
+          stepSize: 1
+        }
+      }
+    ]
+  },
+  legend: {
+    display: false
+  },
+  tooltips: {
+    callbacks: {
+      label: function(tooltipItem: { yLabel: string }) {
+        return tooltipItem.yLabel;
+      }
+    }
+  }
+};
 
 export default {
   name: 'VenueDashboard',
   data() {
     return {
-      venueId: ''
+      venueId: '',
+      venueProfileViews: [],
+      venueCheckIns: [],
+      number: 2,
+      period: 'week'
+    } as {
+      venueId: string;
+      venueProfileViews: viewsBucket[];
+      venueCheckIns: checkInBucket[];
+      number: number;
+      period: 'day' | 'week' | 'month' | 'year';
     };
   },
   mounted() {
@@ -85,6 +144,168 @@ export default {
 
     (this as any).initProfileViewsChart();
     (this as any).initCheckInsChart();
+  },
+  computed: {
+    viewsChartData() {
+      let viewsPerDay: viewsBucket = {};
+      for (let i = 0; i < (this as any).venueProfileViews.length; i++) {
+        let view = (this as any).venueProfileViews[i];
+        let dateString = view.timestamp.toDateString();
+        if (dateString in viewsPerDay) {
+          viewsPerDay[dateString] += 1;
+        } else {
+          viewsPerDay[dateString] = 1;
+        }
+      }
+
+      // get minimum date
+      let minDate = dayjs()
+        .subtract((this as any).number, (this as any).period)
+        .toDate();
+
+      const chartData = [];
+      let now = new Date();
+
+      for (let d = minDate; d <= now; d.setDate(d.getDate() + 1)) {
+        chartData.push({
+          x: new Date(d),
+          y: viewsPerDay[d.toDateString()] || 0
+        });
+      }
+      return chartData;
+    },
+    checkInsChartData() {
+      let checkInsPerDay: checkInBucket = {};
+
+      for (let i = 0; i < (this as any).venueCheckIns.length; i++) {
+        let checkIn = (this as any).venueCheckIns[i];
+        let dateString = checkIn.timeStamp.toDateString();
+        if (dateString in checkInsPerDay) {
+          checkInsPerDay[dateString] += 1;
+        } else {
+          checkInsPerDay[dateString] = 1;
+        }
+      }
+
+      // get minimum date
+      let minDate = dayjs()
+        .subtract((this as any).number, (this as any).period)
+        .toDate();
+
+      const chartData = [];
+
+      let now = new Date();
+      for (let d = minDate; d <= now; d.setDate(d.getDate() + 1)) {
+        chartData.push({
+          x: new Date(d),
+          y: checkInsPerDay[d.toDateString()] || 0
+        });
+      }
+
+      return chartData;
+    },
+    conversionChartData() {
+      let checkInsPerDay: checkInBucket = {};
+      for (let i = 0; i < (this as any).venueCheckIns.length; i++) {
+        let checkIn = (this as any).venueCheckIns[i];
+        let dateString = checkIn.timeStamp.toDateString();
+        if (dateString in checkInsPerDay) {
+          checkInsPerDay[dateString] += 1;
+        } else {
+          checkInsPerDay[dateString] = 1;
+        }
+      }
+
+      let viewsPerDay: viewsBucket = {};
+      for (let i = 0; i < (this as any).venueProfileViews.length; i++) {
+        let view = (this as any).venueProfileViews[i];
+        let dateString = view.timestamp.toDateString();
+        if (dateString in viewsPerDay) {
+          viewsPerDay[dateString] += 1;
+        } else {
+          viewsPerDay[dateString] = 1;
+        }
+      }
+
+      // get minimum date
+      let minDate = dayjs()
+        .subtract((this as any).number, (this as any).period)
+        .toDate();
+
+      const chartData = [];
+
+      let now = new Date();
+
+      for (let d = minDate; d <= now; d.setDate(d.getDate() + 1)) {
+        chartData.push({
+          x: new Date(d),
+          y:
+            viewsPerDay[d.toDateString()] !== 0
+              ? checkInsPerDay[d.toDateString()] /
+                  viewsPerDay[d.toDateString()] || 0
+              : 0
+        });
+      }
+
+      return chartData;
+    }
+  },
+  watch: {
+    viewsChartData(chartData: any) {
+      new Chart('profile-views-chart', {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              data: chartData,
+              backgroundColor: '#26a69a'
+            }
+          ]
+        },
+        options: chartOptions
+      });
+    },
+    checkInsChartData(chartData: any) {
+      new Chart('check-ins-chart', {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              data: chartData,
+              backgroundColor: '#26a69a'
+            }
+          ]
+        },
+        options: chartOptions
+      });
+    },
+    conversionChartData(chartData: any) {
+      new Chart('conversions-chart', {
+        type: 'line',
+        data: {
+          datasets: [
+            {
+              data: chartData,
+              backgroundColor: '#26a69a'
+            }
+          ]
+        },
+        options: {
+          ...chartOptions,
+          scales: {
+            ...chartOptions.scales,
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                  stepSize: 0.25
+                }
+              }
+            ]
+          }
+        }
+      });
+    }
   },
   methods: {
     initProfileViewsChart() {
@@ -103,89 +324,7 @@ export default {
           }
         });
 
-        type viewsBucket = {
-          [key: string]: number;
-        };
-
-        let viewsPerDay: viewsBucket = {};
-        for (let i = 0; i < venueProfileViews.length; i++) {
-          let view = venueProfileViews[i];
-          let dateString = view.timestamp.toDateString();
-          if (dateString in viewsPerDay) {
-            viewsPerDay[dateString] += 1;
-          } else {
-            viewsPerDay[dateString] = 1;
-          }
-        }
-
-        // get minimum date
-        let minDate = new Date();
-        let days = Object.keys(viewsPerDay);
-        for (let i = 0; i < days.length; i++) {
-          let parsed = new Date(days[i]);
-          if (parsed < minDate) {
-            minDate = parsed;
-          }
-        }
-
-        const chartData = [];
-        let now = new Date();
-        for (let d = minDate; d <= now; d.setDate(d.getDate() + 1)) {
-          chartData.push({
-            x: new Date(d),
-            y: viewsPerDay[d.toDateString()]
-          });
-        }
-
-        console.log(chartData);
-        const ctx = document.getElementById('profile-views-chart') as any;
-        const myChart = new Chart(ctx, {
-          type: 'bar',
-          data: {
-            datasets: [
-              {
-                label: 'Daily Venue Views',
-                data: chartData,
-                backgroundColor: [
-                  'rgba(0, 0, 200, 0.5)',
-                ],
-                borderColor: [
-                  'rgba(0, 0, 200, 0.5)',
-                ],
-              }
-            ]
-          },
-          options: {
-            scales: {
-              xAxes: [
-                {
-                  type: 'time',
-                  time: {
-                    unit: 'day'
-                  }
-                }
-              ],
-              yAxes: [
-                {
-                  display: false,
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }
-              ]
-            },
-            legend: {
-              display: false
-            },
-            tooltips: {
-              callbacks: {
-                label: function(tooltipItem: { yLabel: string }) {
-                  return tooltipItem.yLabel;
-                }
-              }
-            }
-          }
-        }) as any;
+        (this as any).venueProfileViews = venueProfileViews;
       });
     },
     initCheckInsChart() {
@@ -204,90 +343,7 @@ export default {
           }
         });
 
-        type checkInBucket = {
-          [key: string]: number;
-        };
-
-        let checkInsPerDay: checkInBucket = {};
-        console.log(venueCheckIns);
-        for (let i = 0; i < venueCheckIns.length; i++) {
-          let checkIn = venueCheckIns[i];
-          let dateString = checkIn.timeStamp.toDateString();
-          if (dateString in checkInsPerDay) {
-            checkInsPerDay[dateString] += 1;
-          } else {
-            checkInsPerDay[dateString] = 1;
-          }
-        }
-
-        // get minimum date
-        let minDate = new Date();
-        let days = Object.keys(checkInsPerDay);
-        for (let i = 0; i < days.length; i++) {
-          let parsed = new Date(days[i]);
-          if (parsed < minDate) {
-            minDate = parsed;
-          }
-        }
-
-        const chartData = [];
-        let now = new Date();
-        for (let d = minDate; d <= now; d.setDate(d.getDate() + 1)) {
-          chartData.push({
-            x: new Date(d),
-            y: checkInsPerDay[d.toDateString()]
-          });
-        }
-
-        console.log(chartData);
-        const ctx = document.getElementById('check-ins-chart') as any;
-        const myChart = new Chart(ctx, {
-          type: 'bar',
-          data: {
-            datasets: [
-              {
-                label: "'Daily Venue Check In's",
-                data: chartData,
-                backgroundColor: [
-                  'rgba(0, 0, 200, 0.5)',
-                ],
-                borderColor: [
-                  'rgba(0, 0, 200, 0.5)',
-                ],
-              }
-            ]
-          },
-          options: {
-            scales: {
-              xAxes: [
-                {
-                  type: 'time',
-                  time: {
-                    unit: 'day'
-                  }
-                }
-              ],
-              yAxes: [
-                {
-                  display: false,
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }
-              ]
-            },
-            legend: {
-              display: false
-            },
-            tooltips: {
-              callbacks: {
-                label: function(tooltipItem: { yLabel: string }) {
-                  return tooltipItem.yLabel;
-                }
-              }
-            }
-          }
-        }) as any;
+        (this as any).venueCheckIns = venueCheckIns;
       });
     }
   }
